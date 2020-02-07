78d9dbb44e3b555af561ef072c10ebbe5090d963
Dennis Aumiller <dennis.aumiller@gmail.com>
2020-02-07 13:55:13 +0100

    Updating main to reflect GPU stuff.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from torch.autograd import Variable
 from model import OVRNet, ReferenceNet
 
 
-def validation_loss(test_loader, net, loss):
+def validation_loss(test_loader, net, loss, device="cpu"):
     net.eval()
     losses = []
 
@@ -23,8 +23,8 @@ def validation_loss(test_loader, net, loss):
     total = 0
 
     for image, label in test_loader:
-        images = Variable(image)
-        labels = Variable(t.LongTensor(label))
+        images = image.to(device)
+        labels = label.to(device)
 
         optimizer.zero_grad()
         outputs = net(images)
@@ -46,15 +46,17 @@ def validation_loss(test_loader, net, loss):
 
 if __name__ == "__main__":
 
-    train = datasets.MNIST(root="./data", transform=tv.transforms.ToTensor(), train=True)
-    test = datasets.MNIST(root="./data", transform=tv.transforms.ToTensor(), train=False)
+    train = datasets.MNIST(root="./data", transform=tv.transforms.ToTensor(), train=True, download=True)
+    test = datasets.MNIST(root="./data", transform=tv.transforms.ToTensor(), train=False, download=True)
 
     loader = DataLoader(train, batch_size=64, shuffle=True)
 
-    test_loader = DataLoader(test, batch_size=1, shuffle=False)
-
+    test_loader = DataLoader(test, batch_size=16, shuffle=False)
+    device = t.device("cuda:0" if t.cuda.is_available() else "cpu")
     net = OVRNet()
+    net.to(device)
     loss = nn.CrossEntropyLoss()
+    loss.to(device)
     optimizer = t.optim.Adam(net.parameters(), lr=0.001)
 
     print("Starting with training.")
@@ -65,19 +67,19 @@ if __name__ == "__main__":
         start = time.time()
         best = 10e6
 
-        # for (image, label) in loader:
-        #     images = Variable(image)
-        #     labels = Variable(label)
-        #
-        #     optimizer.zero_grad()
-        #     outputs = net(images)
-        #
-        #     current_loss = loss(outputs, labels)
-        #     current_loss.backward()
-        #
-        #     optimizer.step()
+        for (image, label) in loader:
+             images = image.to(device)
+             labels = label.to(device)
+        
+             optimizer.zero_grad()
+             outputs = net(images)
+        
+             current_loss = loss(outputs.to(device), labels)
+             current_loss.backward()
+        
+             optimizer.step()
 
         print("Finished with training set, starting validation.")
-        validation_loss(test_loader, net, loss)
+        validation_loss(test_loader, net, loss, device)
         end = time.time()
         print("Epoch {} took {:2f} s".format(epoch, end-start))
